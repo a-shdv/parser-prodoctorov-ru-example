27e468edc5ac8cba3134d485fddfd278ec75473f
a-shdv <shadaev2001@icloud.com>
2024-05-04 02:56:24 +0400

    Test

--- clinic.py
+++ clinic.py
@@ -3,13 +3,15 @@ from ordered_set import OrderedSet
 
 class Clinic:
     name: str
+    city: str
     url: str
     doctors: OrderedSet
 
-    def __init__(self, name: str = "", url: str = "", doctors=None):
+    def __init__(self, name: str = "", city: str = "", url: str = "", doctors=None):
         if doctors is None:
             doctors = OrderedSet()
         self._name = name
+        self._city = city
         self._url = url
         self._doctors = doctors
 
@@ -18,11 +20,18 @@ class Clinic:
         return self._name
 
     @property
+    def get_city(self) -> str:
+        return self._city
+
+    @property
     def get_url(self) -> str:
         return self._url
 
     def set_name(self, _name):
         self._name = _name
+
+    def set_city(self, _city):
+        self._city = _city
 
     def set_url(self, _url):
         self._url = _url
@@ -33,4 +42,3 @@ class Clinic:
 
     def __str__(self):
         return self._name.strip()
-
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import time
+
 import requests
 import openpyxl
 
 from bs4 import BeautifulSoup
+from openpyxl.styles import Font
 from ordered_set import OrderedSet
 
 from clinic import Clinic
@@ -20,67 +23,81 @@ def main():
         clinics_set = OrderedSet()
 
         current_page: int = 1
-        while current_page <= 1:
+        while current_page <= max_number_of_pages:
+            if soup.find("div", class_="appointments_page b-container") is not None:
+                clinics_container = soup.find("div", class_="appointments_page b-container")
 
-            clinics_container = soup.find("div", class_="appointments_page b-container")
-            clinics_cards = clinics_container.find_all("div", class_="b-card")
+                if clinics_container.find_all("div", class_="b-card") is not None:
+                    clinics_cards = clinics_container.find_all("div", class_="b-card")
 
-            for clinic_card in clinics_cards:
-                clinic = Clinic()
-                clinic_name: str = clinic_card.find("a",
-                                                    class_="b-link b-link_underline_hover b-link_color_primary-blue d-inline").find(
-                    "span").get_text()
-                clinic_url: str = ("https://prodoctorov.ru"
-                                   + clinic_card.find("a",
-                                                      class_="b-link b-link_underline_hover b-link_color_primary-blue d-inline")
-                                   .get("href")) + "vrachi/#tab-content"
-                clinic.set_name(clinic_name)
-                clinic.set_url(clinic_url)
+                    for clinic_card in clinics_cards:
+                        clinic = Clinic()
+                        if clinic_card.find("a", class_="b-link b-link_underline_hover b-link_color_primary-blue d-inline").find(
+                            "span") is not None:
+                            clinic_name: str = clinic_card.find("a",
+                                                                class_="b-link b-link_underline_hover b-link_color_primary-blue d-inline").find(
+                                "span").get_text()
 
-                response = requests.get(clinic_url)
-                soup = BeautifulSoup(response.content, "html.parser")
-                doctors_container = soup.findAll("div", class_="b-doctor-card")
+                            if clinic_card.find("a", class_="b-link b-link_underline_hover b-link_color_primary-blue d-inline") is not None:
 
-                for doctor in doctors_container:
-                    doc_to_add = Doctor()
+                                clinic_url: str = ("https://prodoctorov.ru"
+                                                   + clinic_card.find("a",
+                                                                      class_="b-link b-link_underline_hover b-link_color_primary-blue d-inline")
+                                                   .get("href")) + "vrachi/#tab-content"
 
-                    doc_name: str
-                    doc_prof: str
-                    doc_exp: str
-                    doc_url: str
+                                if soup.find("div", "b-text-unit b-text-unit_vertical_middle") is not None:
+                                    clinic_city = soup.find("div", "b-text-unit b-text-unit_vertical_middle").get_text().strip()
+                                else:
+                                    clinic_city = "-"
+                                clinic.set_name(clinic_name)
+                                clinic.set_city(clinic_city)
+                                clinic.set_url(clinic_url)
 
-                    if doctor.find("span", class_="b-doctor-card__name-surname") is not None:
-                        doc_name = doctor.find("span", class_="b-doctor-card__name-surname").get_text()
-                    else:
-                        doc_name = "-"
+                                response = requests.get(clinic_url)
+                                soup = BeautifulSoup(response.content, "html.parser")
+                                doctors_container = soup.findAll("div", class_="b-doctor-card")
 
-                    if doctor.find("div", class_="b-doctor-card__spec") is not None:
-                        doc_prof = doctor.find("div", class_="b-doctor-card__spec").get_text()
-                    else:
-                        doc_prof = "-"
+                                for doctor in doctors_container:
+                                    doc_to_add = Doctor()
 
-                    if doctor.find("div", class_="b-doctor-card__experience-years") is not None:
-                        doc_exp = doctor.find("div", class_="b-doctor-card__experience-years").get_text()
-                    else:
-                        doc_exp = "-"
+                                    doc_name: str
+                                    doc_prof: str
+                                    doc_exp: str
+                                    doc_url: str
 
-                    if doctor.find("a", class_="b-doctor-card__name-link") is not None:
-                        doc_url = doctor.find("a", class_="b-doctor-card__name-link")
-                    else:
-                        doc_url = "-"
+                                    if doctor.find("span", class_="b-doctor-card__name-surname") is not None:
+                                        doc_name = doctor.find("span", class_="b-doctor-card__name-surname").get_text()
+                                    else:
+                                        doc_name = "-"
 
-                    init_doctor(doc_to_add, doc_name, doc_prof, doc_exp, doc_url)
-                    clinic.get_doctors.add(doc_to_add)
+                                    if doctor.find("div", class_="b-doctor-card__spec") is not None:
+                                        doc_prof = doctor.find("div", class_="b-doctor-card__spec").get_text()
+                                    else:
+                                        doc_prof = "-"
 
-                clinics_set.add(clinic)
-                print("=========================================")
-                # print(clinic_url)
-                # print(len(doctors_set))
-                for i, e in enumerate(clinics_set):
-                    print(e.get_doctors)
-                    for val in e.get_doctors:
-                        print(val)
-                print("=========================================")
+                                    if doctor.find("div", class_="b-doctor-card__experience-years") is not None:
+                                        doc_exp = doctor.find("div", class_="b-doctor-card__experience-years").get_text()
+                                    else:
+                                        doc_exp = "-"
+
+                                    if doctor.find("a", class_="b-doctor-card__name-link") is not None:
+                                        doc_url = "https://prodoctorov.ru" + doctor.find("a", class_="b-doctor-card__name-link").get("href")
+                                    else:
+                                        doc_url = "-"
+
+                                    init_doctor(doc_to_add, doc_name, doc_prof, doc_exp, doc_url)
+                                    clinic.get_doctors.add(doc_to_add)
+
+                                clinics_set.add(clinic)
+                                print(current_page)
+                # print("=========================================")
+                # # print(clinic_url)
+                # # print(len(doctors_set))
+                # for i, e in enumerate(clinics_set):
+                #     print(e.get_doctors)
+                #     for val in e.get_doctors:
+                #         print(val)
+                # print("=========================================")
 
             current_page += 1
             response = requests.get(clinics_url.replace("?page=1", f"?page={current_page}"))
@@ -125,17 +142,29 @@ def write_to_excel_file(clinics_set: OrderedSet):
     sheet['F1'] = 'Ссылка на клинику'
     sheet['G1'] = 'Ссылка на врача'
 
-    row = 1
-    j = 1
+    # Set bold font for cells A1 to G1
+    bold_font = Font(bold=True)
+    for col in range(1, 8):  # Columns A to G
+        sheet.cell(row=1, column=col).font = bold_font
+
+    # Set column widths
+    column_widths = [20, 30, 20, 30, 15, 40, 40]  # Adjust as needed
+    for i, width in enumerate(column_widths, start=1):
+        sheet.column_dimensions[openpyxl.utils.get_column_letter(i)].width = width
+
+    row = 2
     while row < len(clinics_set):
 
         for clinic in clinics_set:
             for doc in clinic.get_doctors:
-                sheet['A' + str(row)] = "-"  # Город
-                sheet['B' + str(row)] = clinics_set.__getitem__(j).get_name.strip()  # Клиника
+                sheet['A' + str(row)] = clinic.get_city  # Город
+                sheet['B' + str(row)] = clinic.get_name.strip()  # Клиника
                 sheet['C' + str(row)] = doc.get_name()  # Врач
                 sheet['D' + str(row)] = doc.get_profession()  # Специализация
                 sheet['E' + str(row)] = doc.get_experience()  # Стаж
+                sheet['F' + str(row)] = str(clinic.get_url).strip()  # Ссылка на клинику
+                sheet['G' + str(row)] = str(doc.get_url()).strip()  # Ссылка на врача
+
                 row += 1
         # for doc in clinics_set.__getitem__(row).get_doctors:
         #     # clinics_set.__getitem__(1).get_doctors
